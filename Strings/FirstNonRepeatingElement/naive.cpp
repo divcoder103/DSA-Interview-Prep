ed6d71dd917b0b96085d5b56c12d5b412323e1e2
divcoder103 <divyanshu.hb10@gmail.com>
2026-01-15 20:30:11 +0530

    Create naive.cpp

---
+++ Strings/FirstNonRepeatingElement/naive.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        for(int i=0; i<s.length();i++){
+            boolean flag = false;
+            for(int j=0; j<s.length();j++){
+                if(i!=j && s.charAt(i)==s.charAt(j)){
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag==false)return i;
+        }
+        return -1;
+    }
+}
